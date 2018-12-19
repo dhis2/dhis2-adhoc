46dde434c47770ef8829948b61e6f5d34872f6a8
Lars Helge Øverland <lars@dhis2.org>
2018-12-19 11:40:21 +0100

    Upgraded to 2.32-SNAPSHOT

--- src/main/java/org/hisp/dhis/adhoc/command/RandomCompleteRegistrationPopulator.java
+++ src/main/java/org/hisp/dhis/adhoc/command/RandomCompleteRegistrationPopulator.java
@@ -33,11 +33,11 @@ public class RandomCompleteRegistrationPopulator
     private static final Log log = LogFactory.getLog( RandomCompleteRegistrationPopulator.class );
 
     private static final int OU_LEVEL = 4;
-    
+
     private static final Map<String, Double> DATA_SETS = ImmutableMap.<String, Double>builder().
         put( "TuL8IOPzpHh", 0.85 ).put( "Rl58JxmKJo2", 0.85 ).put( "PLq9sJluXvc", 0.76 ).put( "EDzMBk0RRji", 0.94 ).
         build();
-    
+
     private static final Map<String, Double> PERIODS = ImmutableMap.<String, Double>builder().
         put( "201501", 0.68 ).put( "201502", 0.71 ).put( "201503", 0.78 ).put( "201504", 0.72 ).
         put( "201505", 0.67 ).put( "201506", 0.82 ).put( "201507", 0.89 ).put( "201508", 0.92 ).
@@ -45,13 +45,13 @@ public class RandomCompleteRegistrationPopulator
         put( "201601", 0.67 ).put( "201602", 0.69 ).put( "201603", 0.73 ).put( "201604", 0.79 ).
         put( "201605", 0.84 ).put( "201606", 0.89 ).put( "201607", 0.98 ).put( "201608", 0.96 ).
         put( "201609", 0.92 ).put( "201610", 0.80 ).put( "201611", 0.78 ).put( "201612", 0.83 ).build();
-    
+
     @Autowired
     private IdentifiableObjectManager idObjectManager;
 
     @Autowired
     private PeriodService periodService;
-    
+
     @Autowired
     private BatchHandlerFactory batchHandlerFactory;
 
@@ -66,9 +66,9 @@ public class RandomCompleteRegistrationPopulator
         List<DataSet> dss = idObjectManager.getObjects( DataSet.class, IdentifiableProperty.UID, DATA_SETS.keySet() );
 
         Date date = new Date();
-        
+
         String storedBy = "admin";
-        
+
         // ---------------------------------------------------------------------
         // Organisation units
         // ---------------------------------------------------------------------
@@ -80,7 +80,7 @@ public class RandomCompleteRegistrationPopulator
         // ---------------------------------------------------------------------
 
         List<Period> pes = periodService.reloadIsoPeriods( Lists.newArrayList( PERIODS.keySet() ) );
-        
+
         log.info( "Periods: " + pes );
 
         BatchHandler<CompleteDataSetRegistration> batchHandler = batchHandlerFactory.createBatchHandler( CompleteDataSetRegistrationBatchHandler.class ).init();
@@ -92,28 +92,28 @@ public class RandomCompleteRegistrationPopulator
                 for ( Period pe : pes )
                 {
                     Double weight = DATA_SETS.get( ds.getUid() ) * PERIODS.get( pe.getIsoDate() );
-                    
+
                     Double ouMax = ous.size() * weight;
-                    
+
                     log.info( "Org unit max: " + ouMax );
-                                        
+
                     Collections.shuffle( ous );
                     List<OrganisationUnit> subOus = ListUtils.subList( ous, 0, ouMax.intValue() );
-                    
+
                     for ( OrganisationUnit ou : subOus )
                     {
-                        CompleteDataSetRegistration cdr = new CompleteDataSetRegistration( ds, pe, ou, aoc, date, storedBy );
-                        
-                        batchHandler.addObject( cdr );                
+                        CompleteDataSetRegistration cdr = new CompleteDataSetRegistration( ds, pe, ou, aoc, date, storedBy, date, storedBy, true );
+
+                        batchHandler.addObject( cdr );
                     }
                 }
             }
-            
+
             log.info( "Done for data set: " + ds );
         }
-        
+
         batchHandler.flush();
-        
+
         log.info( "Registration population completed" );
     }
 }
